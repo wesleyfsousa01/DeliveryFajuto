f6456d849809288edbedb676739232e168a13190
Wesley Ferreira <103182935+wesleyfsousa01@users.noreply.github.com>
2022-12-13 11:53:14 -0300

    Add files via upload

---
+++ Delivery001/src/MODEL/Usuario.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MODEL;
+
+/**
+ *
+ * @author Afranio
+ */
+public class Usuario {
+    public int id;
+    public String nome;
+    public  String senha;
+    public  String email;
+    public  int tipo;
+    
+}
